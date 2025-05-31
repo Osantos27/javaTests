001c88ca36424f6a1f05efc0a6e6725d250841ad
Santos <154452367+Osantos27@users.noreply.github.com>
2025-05-31 16:10:21 +0100

    Add files via upload

--- test2/src/main/java/com/test2/DoubleJumpListener.java
+++ test2/src/main/java/com/test2/DoubleJumpListener.java
@@ -1,6 +1,9 @@
 package com.test2;
 
+import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -10,8 +13,14 @@ import org.bukkit.event.player.PlayerToggleFlightEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.concurrent.SynchronousQueue;
+
 public final class DoubleJumpListener extends JavaPlugin implements Listener {
 
+
+    private final HashMap<UUID, Long> lastJumpTime = new HashMap<>();
 
     @Override
     public void onEnable() {
@@ -36,10 +45,35 @@ public final class DoubleJumpListener extends JavaPlugin implements Listener {
         }
 
         if (!player.isFlying()){
+
+
             playerToggleFlightEvent.setCancelled(true);
+
+
+            if (lastJumpTime.containsKey(player.getUniqueId())){
+                long timeElapsed = System.currentTimeMillis() - lastJumpTime.get(player.getUniqueId());
+                if (timeElapsed < 5000 ){
+                    double remainingSeconds = Math.ceil((5000 - timeElapsed) / 100.0);
+                    player.sendMessage(ChatColor.RED + "The ability is in cooldown for" + ChatColor.DARK_RED + remainingSeconds + ChatColor.RED + "!!!");
+                    return;
+                }
+            }
+
+
+
             player.setAllowFlight(false);
+
             player.setFlying(false);
+
             player.setVelocity(player.getLocation().getDirection().multiply(1.2).setY(0.8));
+
+            player.getWorld().spawnParticle(Particle.CLOUD, player.getLocation(), 30, 0.5, 0.1, 0.5, 0.1);
+
+            // Add sound effect
+            player.getWorld().playSound(player.getLocation(), Sound.ENTITY_BAT_TAKEOFF, 1.0f, 1.0f);
+
+
+            lastJumpTime.put(player.getUniqueId(), System.currentTimeMillis());
         }
     }
 
